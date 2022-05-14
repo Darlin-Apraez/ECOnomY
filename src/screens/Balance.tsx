a7f13397488beeb5ebc92e76218daef9529141ce
DarlinCondC <darlinapraez@condorcoin.co>
2022-05-14 09:12:55 -0500

    system button lock is added on screens Register and Balance

--- src/screens/Balance.tsx
+++ src/screens/Balance.tsx
@@ -1,5 +1,12 @@
-import { View, Text, SafeAreaView, StatusBar } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  Alert,
+  BackHandler,
+} from "react-native";
+import React, { useEffect } from "react";
 import {
   stylesB,
   stylesL,
@@ -9,6 +16,26 @@ import {
 } from "./../appTheme/styles/styles";
 
 const Balance = () => {
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert("Espera", "¿Seguro que quieres salir?", [
+        {
+          text: "Cancelar",
+          onPress: () => null,
+          style: "cancel",
+        },
+        { text: "Si", onPress: () => BackHandler.exitApp() },
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
   return (
     <SafeAreaView>
       <StatusBar backgroundColor={"black"} barStyle={"light-content"} />
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -4,35 +4,64 @@ import {
   SafeAreaView,
   StatusBar,
   TouchableOpacity,
-  Image
+  Image,
+  Alert,
+  BackHandler,
 } from "react-native";
-import React from "react";
-import { stylesM, stylesB } from '../appTheme/styles/styles';
+import React, { useEffect } from "react";
+import { stylesM, stylesB } from "../appTheme/styles/styles";
 
 const Register = ({ navigation }: { navigation: any }) => {
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert("Espera", "¿Seguro que quieres salir?", [
+        {
+          text: "Cancelar",
+          onPress: () => null,
+          style: "cancel",
+        },
+        { text: "Si", onPress: () => BackHandler.exitApp() },
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
   return (
     <SafeAreaView style={stylesB.body}>
-
       <StatusBar backgroundColor={"black"} barStyle={"light-content"} />
-      <View style={[stylesB.completo,
-      stylesM.BacColor,
-      ]}>
+      <View style={[stylesB.completo, stylesM.BacColor]}>
         {/* <View style={stylesB.completo}>  */}
         {/* <View style={{backgroundColor:"red"}}> */}
         <View>
           {/* <View> */}
-          <Image source={require("../../assets/img/ECOnomY.Name.png")} style={stylesM.NameScale}/>
-         {/* </View> */}
-          <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")} style={stylesM.buttonCreate}>
-          <Text style={{fontSize:20, marginLeft: 25,}}>CREAR CUENTA</Text>
-         </TouchableOpacity>
-         <TouchableOpacity onPress={() => navigation.navigate("Login")} style={stylesM.buttonCreate}>
-          <Text style={{fontSize:20, marginLeft: 25,}}>INICIAR SESIÓN</Text>
-        </TouchableOpacity>
-      </View>
-      {/* </View> */}
+          <Image
+            source={require("../../assets/img/ECOnomY.Name.png")}
+            style={stylesM.NameScale}
+          />
+          {/* </View> */}
+          <TouchableOpacity
+            onPress={() => navigation.navigate("CreateAccount")}
+            style={stylesM.buttonCreate}
+          >
+            <Text style={{ fontSize: 20, marginLeft: 25 }}>CREAR CUENTA</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Login")}
+            style={stylesM.buttonCreate}
+          >
+            <Text style={{ fontSize: 20, marginLeft: 25 }}>INICIAR SESIÓN</Text>
+          </TouchableOpacity>
+        </View>
+        {/* </View> */}
 
-      {/* </View> */}
+        {/* </View> */}
       </View>
     </SafeAreaView>
   );
